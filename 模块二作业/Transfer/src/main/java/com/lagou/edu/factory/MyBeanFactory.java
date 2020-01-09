613f990444e69d06f1d198a0beab561322a64401
Li-Ping <1635335126@qq.com>
2020-01-09 15:00:20 +0800

    代码题修改

--- 模块二作业/Transfer/src/main/java/com/lagou/edu/factory/MyBeanFactory.java
+++ 模块二作业/Transfer/src/main/java/com/lagou/edu/factory/MyBeanFactory.java
@@ -6,6 +6,9 @@ import com.lagou.edu.annotation.Service;
 import com.lagou.edu.annotation.Transactional;
 import com.lagou.edu.utils.FindClassFileUtils;
 import com.lagou.edu.utils.TransactionManager;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
 import org.reflections.Reflections;
 import org.reflections.scanners.FieldAnnotationsScanner;
 
@@ -33,13 +36,13 @@ public class MyBeanFactory {
             for (Class<?> aClass : service) {
                 Object bean = bean = aClass.newInstance();
                 Service annotation = aClass.getAnnotation(Service.class);
+                // 判断注解有无value属性值的情况
                 if (!"".equals(annotation.value())){
                     map.put(annotation.value(), bean);
                 }else{
                     String beanName = getBeanName(aClass.getName());
                     map.put(beanName, bean);
                 }
-
             }
 
             // 获得此路径下所有带有自定义Component注解的类
@@ -48,6 +51,7 @@ public class MyBeanFactory {
             for (Class<?> aClass : component) {
                 Object bean = bean = aClass.newInstance();
                 Component annotation = aClass.getAnnotation(Component.class);
+                // 判断注解有无value属性值的情况
                 if (!"".equals(annotation.value())){
                     map.put(annotation.value(), bean);
                 }else{
@@ -100,9 +104,13 @@ public class MyBeanFactory {
             Object o = map.get(getBeanName(getValue("com.lagou.edu.utils.TransactionManager")));
             // 将这些类通过反射 新建对象并保存到map
             for (Class<?> aClass : transactional) {
-                System.out.println(aClass.getName());
                 Object bean = map.get(getBeanName(getValue(aClass.getName())));
-                bean = getJdkProxy((TransactionManager) o,bean);
+                // 判断service层是否实现接口的情况【jdk还是cglib】
+                if (bean.getClass().getInterfaces().length > 0){
+                    bean = getJdkProxy((TransactionManager) o,bean);
+                }else{
+                    bean = getCglibProxy((TransactionManager) o,bean);
+                }
                 // 把处理之后的对象重新放到map中
                 map.put(getBeanName(getValue(aClass.getName())), bean);
             }
@@ -173,6 +181,34 @@ public class MyBeanFactory {
                     // 开启事务(关闭事务的自动提交)
                     transactionManager.beginTransaction();
                     ob = method.invoke(o, args);
+                    // 提交事务
+                    transactionManager.commit();
+                }catch (Exception e) {
+                    e.printStackTrace();
+                    // 回滚事务
+                    transactionManager.rollback();
+                    // 抛出异常便于上层servlet捕获
+                    throw e;
+                }
+                return ob;
+            }
+        });
+    }
+
+    /**
+     * 使用CGLIB动态代理增强事务控制
+     * @param o
+     * @return
+     */
+    public static Object getCglibProxy(TransactionManager transactionManager,Object o){
+        return Enhancer.create(o.getClass(), new MethodInterceptor() {
+            @Override
+            public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+                Object ob = null;
+                try{
+                    // 开启事务(关闭事务的自动提交)
+                    transactionManager.beginTransaction();
+                    ob = method.invoke(o, objects);
                     // 提交事务
                     transactionManager.commit();
                 }catch (Exception e) {
